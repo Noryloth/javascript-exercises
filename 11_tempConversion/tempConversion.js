2b2e8640033d39c2ee59b5c846c48d71c68b7e5f
Katsuya <noryloth@gmail.com>
2025-05-29 18:25:15 +0800

    Finish exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let toCelsius = (temp - 32) * 5/9;
+  let resultCelsius = Math.round(toCelsius * 10) / 10;
+  return resultCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let toFahrenheit = (temp * 9/5 + 32);
+  let resultFahrenheit = Math.round(toFahrenheit * 10) / 10;
+  return resultFahrenheit;
 };
 
 // Do not edit below this line
