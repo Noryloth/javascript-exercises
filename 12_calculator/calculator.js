f89b4f3f835fe1c2e34da875807511146abd784e
Katsuya <noryloth@gmail.com>
2025-06-03 17:53:44 +0800

    Finish exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let num of array) {
+	  sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mult = 1;
+  for (let num of array) {
+    mult *= num;
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let result = num1;
+	if (num1 === 0 || num1 === 1) {
+    return 1;
+  } else {
+    while (num1 > 1) {
+      num1--;
+      result = result * num1;
+    }
+    return result;
+    }
 };
 
 // Do not edit below this line
