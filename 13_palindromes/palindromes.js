8ac86ca0fb7c064208821f6d5d49530d88e040c1
Katsuya <noryloth@gmail.com>
2025-06-03 18:19:30 +0800

    Finish exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const clean = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
+
+    const reversed = clean.split('').reverse().join('');
+
+    if (clean === reversed) {
+        console.log(reversed);
+        return true;
+    } else {
+        console.log(reversed);
+        return false;
+        
+    }
 };
 
 // Do not edit below this line
